b48534c954851387222f649a2fc19cde878e6a80
Pratyush Sinha <122526404+GitPratyush@users.noreply.github.com>
2025-02-09 23:01:18 +0530

    Intialize a SigleThreaded folder for making single threaded server

---
+++ SingleThreaded/src/Server.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+
+    public void run() throws IOException {
+        int port = 8080;
+        ServerSocket socket= new ServerSocket(port);
+    }
+
+    public static void main(String[] args){
+
+    }
+}
